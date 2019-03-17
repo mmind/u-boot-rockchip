c74e5bacdfa46dbeda82216097602dec428a1373
Heiko Stuebner <heiko@sntech.de>
2019-03-17 23:46:28 +0100

    rockchip: rk3288: make both evb variants use atf
    Enable both rk3288-evb variants to load an ATF binary between
    spl and u-boot proper.
    
    Doing the regular spl->u-boot load of course still stays possible
    with this change.
    
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>

--- board/rockchip/evb_rk3288/evb-rk3288.c
+++ board/rockchip/evb_rk3288/evb-rk3288.c
@@ -16,3 +16,13 @@ void board_boot_order(u32 *spl_boot_list)
 }
 
 #endif
+
+#ifdef CONFIG_SPL_LOAD_FIT
+int board_fit_config_name_match(const char *name)
+{
+	/* Just empty function now - can't decide what to choose */
+	debug("%s: %s\n", __func__, name);
+
+	return 0;
+}
+#endif
