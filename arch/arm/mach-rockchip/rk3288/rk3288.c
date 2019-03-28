639414496091fe727678ca7702be041b516b7bb4
Heiko Stuebner <heiko@sntech.de>
2019-03-28 11:06:46 +0100

    rockchip: rk3288: reserve first 2MB when build with ATF support
    ATF resides in the first 2MB of ram and will also protect this
    area from non-secure access.
    
    So similar to other Rockchip socs keep this area from the usable ram.
    
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>

--- arch/arm/mach-rockchip/rk3288/rk3288.c
+++ arch/arm/mach-rockchip/rk3288/rk3288.c
@@ -2,11 +2,27 @@
 /*
  * Copyright (c) 2016 Rockchip Electronics Co., Ltd
  */
+#include <common.h>
 #include <asm/io.h>
 #include <asm/arch-rockchip/hardware.h>
 #include <asm/arch-rockchip/grf_rk3288.h>
 
+DECLARE_GLOBAL_DATA_PTR;
+
 #define GRF_BASE	0xff770000
+
+int dram_init_banksize(void)
+{
+#ifdef CONFIG_SPL_ATF
+	size_t max_size = min((unsigned long)gd->ram_size, gd->ram_top);
+
+	/* Reserve 0x200000 for ATF bl32 */
+	gd->bd->bi_dram[0].start = 0x0200000;
+	gd->bd->bi_dram[0].size = max_size - gd->bd->bi_dram[0].start;
+#endif
+
+	return 0;
+}
 
 int arch_cpu_init(void)
 {
