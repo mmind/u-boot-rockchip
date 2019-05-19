7c018566b975099606e3e354d3d3d3cfd0740972
Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
2019-05-19 16:39:10 +0200

    rockchip: board: lion-rk3368: enable regulators during bootup
    For the RK3368-uQ7, we want to enable regulators automatically during
    bootup. This adds the required incantations to the board-specific
    implementation to enable regulators at SPL and in the full U-Boot.
    
    Signed-off-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>

--- board/theobroma-systems/lion_rk3368/lion_rk3368.c
+++ board/theobroma-systems/lion_rk3368/lion_rk3368.c
@@ -2,14 +2,17 @@
 /*
  * Copyright (c) 2017 Theobroma Systems Design und Consulting GmbH
  */
+
 #include <common.h>
 #include <dm.h>
 #include <ram.h>
+#include <spl.h>
+#include <syscon.h>
 #include <asm/io.h>
 #include <asm/arch-rockchip/clock.h>
 #include <asm/arch-rockchip/grf_rk3368.h>
 #include <asm/arch-rockchip/timer.h>
-#include <syscon.h>
+#include <power/regulator.h>
 
 int mach_cpu_init(void)
 {
@@ -18,5 +21,28 @@ int mach_cpu_init(void)
 
 int board_init(void)
 {
+	int ret;
+
+	/*
+	 * We need to call into regulators_enable_boot_on() again, as the call
+	 * during SPL may have not included all regulators.
+	 */
+	ret = regulators_enable_boot_on(false);
+	if (ret)
+		debug("%s: Cannot enable boot on regulator\n", __func__);
+
 	return 0;
+}
+
+void spl_board_init(void)
+{
+	int ret;
+
+	debug("%s: calling regulators_enable_boot_on\n", __func__);
+
+	ret = regulators_enable_boot_on(false);
+	if (ret)
+		debug("%s: Cannot enable boot on regulator\n", __func__);
+
+	preloader_console_init();
 }
