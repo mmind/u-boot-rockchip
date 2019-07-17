baa7f162c5491eb850279d23cb7833fe814c2bbe
Heiko Stuebner <heiko.stuebner@theobroma-systems.com>
2019-07-17 11:58:08 +0200

    rockchip: ram: add dm-based sdram driver
    sdram configuration happens outside of dm-infrastructure in special
    tpl-code, so the sdram driver itself has just the function to read
    back the sdram configuration and allow main uboot to handle dram sizes.
    
    Signed-off-by: Heiko Stuebner <heiko.stuebner@theobroma-systems.com>

--- drivers/ram/rockchip/sdram_px30.c
+++ drivers/ram/rockchip/sdram_px30.c
@@ -11,11 +11,6 @@
 #include <asm/arch-rockchip/grf_px30.h>
 #include <asm/arch-rockchip/sdram_common.h>
 
-struct dram_info {
-	struct ram_info info;
-	struct px30_pmugrf *pmugrf;
-};
-
 static int px30_dmc_probe(struct udevice *dev)
 {
 	struct dram_info *priv = dev_get_priv(dev);
